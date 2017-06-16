e505222b9fe6861f7314d50e86de6a610f9a1f44
David Verhasselt <david@crowdway.com>
2017-06-16 14:07:54 +0300

    Fix timeout on Mac

--- lib/docsplit/image_extractor.rb
+++ lib/docsplit/image_extractor.rb
@@ -7,6 +7,7 @@ module Docsplit
     MEMORY_ARGS     = "-limit memory 256MiB -limit map 512MiB"
     DEFAULT_FORMAT  = :png
     DEFAULT_DENSITY = '150'
+    COMMAND_TIMEOUT = 300 # seconds
 
     # Extract a list of PDFs as rasterized page images, according to the
     # configuration in options.
@@ -34,30 +35,56 @@ module Docsplit
       pages     = @pages || '1-' + Docsplit.extract_length(pdf).to_s
       escaped_pdf = ESCAPE[pdf]
       FileUtils.mkdir_p(directory) unless File.exists?(directory)
-      common    = "#{MEMORY_ARGS} -density #{@density} #{resize_arg(size)} #{quality_arg(format)}"
-      timeout   = 5.minutes.to_i
+      env = "MAGICK_TMPDIR=#{tempdir} OMP_NUM_THREADS=2"
+      common = "#{MEMORY_ARGS} -density #{@density} #{resize_arg(size)} #{quality_arg(format)}"
+
       if previous
         FileUtils.cp(Dir[directory_for(previous) + '/*'], directory)
         # We're adding `| grep -v '^$' | uniq` here and below because if a corrupt PDF is parsed, it generates an infinite amount of identical warnings (with blank lines in between).
         # By filtering these we avoid memory bloat when the executing process tries to capture stdout.
         # See https://github.com/GetSilverfin/silverfin/issues/1998
-        cmd = "MAGICK_TMPDIR=#{tempdir} OMP_NUM_THREADS=2 timeout #{timeout} gm mogrify #{common} -unsharp 0x0.5+0.75 \"#{directory}/*.#{format}\" 2>&1 | grep -v \"^$\" | uniq; exit ${PIPESTATUS[0]}"
-        result = `bash -c '#{cmd}'`.chomp
-        raise ExtractionFailed, result if $? != 0
+
+        run(env, "gm mogrify #{common} -unsharp 0x0.5+0.75 \"#{directory}/*.#{format}\" 2>&1")
       else
         page_list(pages).each do |page|
-          out_file  = ESCAPE[File.join(directory, "#{basename}_#{page}.#{format}")]
-          cmd = "MAGICK_TMPDIR=#{tempdir} OMP_NUM_THREADS=2 timeout #{timeout} gm convert +adjoin -define pdf:use-cropbox=true #{common} #{escaped_pdf}[#{page - 1}] #{out_file} 2>&1 | grep -v \"^$\" | uniq; exit ${PIPESTATUS[0]}"
-          result = `bash -c '#{cmd}'`.chomp
-          raise ExtractionFailed, result if $? != 0
+          out_file = ESCAPE[File.join(directory, "#{basename}_#{page}.#{format}")]
+          run(env, "gm convert +adjoin -define pdf:use-cropbox=true #{common} #{escaped_pdf}[#{page - 1}] #{out_file} 2>&1")
         end
       end
     ensure
       FileUtils.remove_entry_secure tempdir if File.exists?(tempdir)
     end
 
-
     private
+
+    # Run an external process and raise an exception if it fails.
+    def run(env, command)
+      # If a corrupt PDF is parsed, it generates an infinite amount of identical warnings (with blank lines in between).
+      # By filtering these we avoid memory bloat when the executing process tries to capture stdout. The timeout makes
+      # sure we exit at some point.
+      #
+      # - See https://github.com/GetSilverfin/silverfin/issues/1998
+      # - Add timeout so a stuck process doesn't block our Ruby process forever
+      # - Remove blank lines
+      # - Remove duplicate lines
+      run_command = "#{env} #{timeout} #{command} | grep -v \"^$\" | uniq"
+
+      # - Run through bash so we can use PIPESTATUS
+      # - Use PIPESTATUS to return the exit status of #{command} instead of `uniq`
+      result = `bash -c '#{run_command}; exit ${PIPESTATUS[0]}'`.chomp
+
+      raise ExtractionFailed, result if $? != 0
+      result
+    end
+
+    def timeout
+      "#{timeout_bin} #{COMMAND_TIMEOUT}"
+    end
+
+    def timeout_bin
+      # gtimeout on Mac
+      `which timeout` != "" ? "timeout" : "gtimeout"
+    end
 
     # Extract the relevant GraphicsMagick options from the options hash.
     def extract_options(options)
